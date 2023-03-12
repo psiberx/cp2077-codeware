42e535344734038cff15f17515599b2e674586fc
pv <p@siberx.dev>
2023-03-12 09:31:27 +0300

    Update build tools

---
+++ tools/dist/install-mod.ps1
@@ -0,0 +1,11 @@
+param ($GameDir, $ReleaseBin, $ProjectName = "Codeware")
+
+$StageDir = "build/package"
+$DistDir = "build/dist"
+$Version = & $($PSScriptRoot + "\steps\get-version.ps1")
+
+& $($PSScriptRoot + "\steps\compose-redscripts.ps1") -StageDir ${StageDir} -ProjectName ${ProjectName} -Version ${Version}
+& $($PSScriptRoot + "\steps\compose-plugin-red4ext.ps1") -StageDir ${StageDir} -ProjectName ${ProjectName} -ReleaseBin ${ReleaseBin}
+& $($PSScriptRoot + "\steps\install-from-stage.ps1") -StageDir ${StageDir} -GameDir ${GameDir}
+
+Remove-Item -Recurse ${StageDir}
