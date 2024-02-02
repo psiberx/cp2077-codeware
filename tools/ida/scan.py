f87e36038c5844deca4b9ae3baeb3f8f46d58768
pv <p@siberx.dev>
2024-02-02 16:06:27 +0300

    Fix raw input address

--- tools/ida/scan.py
+++ tools/ida/scan.py
@@ -76,7 +76,7 @@ def patterns():
                 Item(name="ProcessCharacterEvent",
                      pattern="48 89 5C 24 ? 44 89 44 24 ? 55 48 8B EC 48 83 EC 50 44 8B D2 48 8B D9 48 81 C1 ? ? ? ? 48 8D 55 ? E8"),
                 Item(name="ProcessInputEvents",
-                     pattern="4C 89 44 24 ? 48 89 54 24 ? 55 53 56 57 41 54 41 55 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 48 8B 01 48 8B FA 45 33 F6"),
+                     pattern="48 8B C4 48 89 58 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D 68 ? 48 81 EC ? ? ? ? 0F 29 70 ? 4C 8B EA 0F 29 78 ? 48 8B F9 44 0F 29 40 ?"),
             ]),
             Group(name="InkWidget", functions=[
                 Item(name="GetLayer",
