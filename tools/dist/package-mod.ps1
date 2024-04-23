bd72c0da5b3d3a5bbc30f30c850dff8c51c99aa0
pv <p@siberx.dev>
2024-04-23 07:56:54 +0300

    Update build tools

--- tools/dist/package-mod.ps1
+++ tools/dist/package-mod.ps1
@@ -6,6 +6,7 @@ $Version = & $($PSScriptRoot + "\steps\get-version.ps1")
 
 & $($PSScriptRoot + "\steps\compose-redscripts.ps1") -StageDir ${StageDir} -ProjectName ${ProjectName} -Version ${Version}
 & $($PSScriptRoot + "\steps\compose-plugin-red4ext.ps1") -StageDir ${StageDir} -ProjectName ${ProjectName} -ReleaseBin ${ReleaseBin}
+& $($PSScriptRoot + "\steps\compose-plugin-data.ps1") -StageDir ${StageDir} -ProjectName ${ProjectName}
 & $($PSScriptRoot + "\steps\create-zip-from-stage.ps1") -StageDir ${StageDir} -ProjectName ${ProjectName} -DistDir ${DistDir} -Version ${Version}
 
 Remove-Item -Recurse ${StageDir}
