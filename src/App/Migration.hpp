f70ef6b101f1856d8b30d534418b81124ba7f0b2
pv <p@siberx.dev>
2024-04-27 08:47:41 +0300

    Update build tools

--- src/App/Migration.hpp
+++ src/App/Migration.hpp
@@ -7,7 +7,7 @@ inline void CleanUp(const std::filesystem::path& aPath)
     std::error_code error;
     if (std::filesystem::exists(aPath, error))
     {
-        std::filesystem::remove(aPath, error);
+        std::filesystem::remove_all(aPath, error);
     }
 }
 }
