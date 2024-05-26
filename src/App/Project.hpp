33a6609a8311acad83237401746e8df70da8b9fa
psiberx <psiberx@gmail.com>
2024-05-27 00:11:05 +0300

    Update version

--- src/App/Project.hpp
+++ src/App/Project.hpp
@@ -12,5 +12,5 @@ constexpr auto Author = "psiberx";
 constexpr auto NameW = L"Codeware";
 constexpr auto AuthorW = L"psiberx";
 
-constexpr auto Version = semver::from_string_noexcept("1.9.6").value();
+constexpr auto Version = semver::from_string_noexcept("1.9.7").value();
 }
