f70e028d42f13cbb8d8ee53fdb3e50e5e8f0868e
psiberx <psiberx@gmail.com>
2024-06-14 20:46:42 +0300

    Refactoring

--- src/Red/Debug.hpp
+++ src/Red/Debug.hpp
@@ -3,6 +3,6 @@
 namespace Raw::EntityID
 {
 constexpr auto ToStringDEBUG = Core::RawFunc<
-    /* addr = */ 0x140E75FC4 - Red::Address::ImageBase, // FIXME
+    /* addr = */ 0x0E75FC4, // FIXME
     /* type = */ Red::CString* (*)(const Red::EntityID& aEntityID, Red::CString&)>();
 }
