190b108131a71769b706201dfeedf0b5917c2b2f
psiberx <psiberx@gmail.com>
2025-01-18 03:57:59 +0300

    Code cleanup

--- src/Red/TagSystem.hpp
+++ src/Red/TagSystem.hpp
@@ -8,10 +8,10 @@ using TagSystem = Core::OffsetPtr<0x48, Red::redTagSystem>;
 namespace Raw::TagSystem
 {
 constexpr auto AssignTag = Core::RawFunc<
-    /* addr = */ Red::AddressLib::TagSystem_AssignTag, // 1239092911
+    /* addr = */ Red::AddressLib::TagSystem_AssignTag,
     /* type = */ void (*)(Red::redTagSystem* aSystem, Red::EntityID aEntityID, Red::CName aTag)>();
 
 constexpr auto UnassignTag = Core::RawFunc<
-    /* addr = */ Red::AddressLib::TagSystem_UnassignTag, // 1875775378
+    /* addr = */ Red::AddressLib::TagSystem_UnassignTag,
     /* type = */ void (*)(Red::redTagSystem* aSystem, Red::EntityID aEntityID, Red::CName aTag)>();
 }
